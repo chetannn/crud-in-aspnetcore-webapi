9a2c0f4bcf41d15b803c86f0e89c789d0eda3df9
chetan.kharel <chetankharel7@gmail.com>
2019-01-05 07:30:21 +0545

    add user login.

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,31 +1,64 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
 using PostApi.Models;
 using PostApi.Repository;
+using System;
 
 namespace PostApi.Controllers
 {
-    [Route("api/[controller]")]
-    public class AuthController :ControllerBase
+  [Route("api/[controller]")]
+  public class AuthController : ControllerBase
+  {
+    private readonly IAuthRepository _repo;
+
+    public AuthController(IAuthRepository repo)
     {
-        private readonly IAuthRepository _repo;
-
-        public AuthController(IAuthRepository repo)
-        {
-            _repo = repo;
-        }
-
-        [Route("register")]
-        public async Task<IActionResult> Register([FromBody] User user)
-        { 
-            if(!string.IsNullOrEmpty(user.Username))
-                user.Username = user.Username.ToLower();
-
-                if(!ModelState.IsValid)
-                return BadRequest(ModelState);
-
-                var createdUser = await _repo.Register(user, user.Password);
-                return Ok(createdUser);
-        }
+      _repo = repo;
     }
+
+    [Route("register")]
+    public async Task<IActionResult> Register([FromBody] User user)
+    {
+      if (!string.IsNullOrEmpty(user.Username))
+        user.Username = user.Username.ToLower();
+
+      if (!ModelState.IsValid)
+        return BadRequest(ModelState);
+
+      var createdUser = await _repo.Register(user, user.Password);
+      return Ok(createdUser);
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login([FromBody] User user)
+    {
+      var userFromRepo = await _repo.Login(user.Username.ToLower(), user.Password);
+
+      if (userFromRepo == null)
+        return Unauthorized();
+
+      //generate token
+      var tokenHandler = new JwtSecurityTokenHandler();
+      var key = Encoding.ASCII.GetBytes("super secret key");
+
+      var tokenDescriptor = new SecurityTokenDescriptor
+      {
+        Subject = new ClaimsIdentity(new Claim[] {
+                      new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
+                      new Claim(ClaimTypes.Name, userFromRepo.Username)
+                  }),
+        Expires = DateTime.Now.AddDays(7),
+        SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha512Signature)
+
+      };
+      var token = tokenHandler.CreateToken(tokenDescriptor);
+      var tokenString = tokenHandler.WriteToken(token);
+
+      return Ok(new { tokenString, userFromRepo });
+    }
+  }
 }
--- Repository/AuthRepository.cs
+++ Repository/AuthRepository.cs
@@ -3,47 +3,72 @@ using PostApi.Models;
 using PostApi.Data;
 using System;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace PostApi.Repository
 {
   public class AuthRepository : IAuthRepository
   {
-     private readonly AppDbContext _context;
-     public AuthRepository(AppDbContext context)
+    private readonly AppDbContext _context;
+    public AuthRepository(AppDbContext context)
     {
-        _context = context;
+      _context = context;
     }
-    public Task<User> Login(string username, string password)
+    public async Task<User> Login(string username, string password)
     {
-      throw new System.NotImplementedException();
+      var user = await _context.Users.FirstOrDefaultAsync(x => x.Username == username);
+
+      if (user == null)
+        return null;
+
+      if (!VerifyPasswordHash(password, user.PasswordHash, user.PasswordSalt))
+        return null;
+
+      return user;
+    }
+
+    private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+    {
+      using (var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))
+      {
+        var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+        for (int i = 0; i < computedHash.Length; i++)
+        {
+          if (computedHash[i] != passwordHash[i]) return false;
+        }
+        return true;
+      }
     }
 
     public async Task<User> Register(User user, string password)
     {
-       byte[] passwordHash,passwordSalt;
+      byte[] passwordHash, passwordSalt;
 
-       CreatePasswordHash(password, out passwordHash, out passwordSalt);
-       user.PasswordHash = passwordHash;
-       user.PasswordSalt = passwordSalt;
+      CreatePasswordHash(password, out passwordHash, out passwordSalt);
+      user.PasswordHash = passwordHash;
+      user.PasswordSalt = passwordSalt;
 
-       await _context.Users.AddAsync(user);
-       await _context.SaveChangesAsync();
-       
-       return user;
+      await _context.Users.AddAsync(user);
+      await _context.SaveChangesAsync();
+
+      return user;
     }
 
     private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
     {
-        using(var hmac = new System.Security.Cryptography.HMACSHA512())
-        {
-            passwordSalt = hmac.Key;
-            passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
-        }
+      using (var hmac = new System.Security.Cryptography.HMACSHA512())
+      {
+        passwordSalt = hmac.Key;
+        passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+      }
     }
 
-    public Task<bool> UserExists(string username)
+    public async Task<bool> UserExists(string username)
     {
-      throw new System.NotImplementedException();
+      if (await _context.Users.AnyAsync(x => x.Username == username))
+        return true;
+      else
+        return false;
     }
   }
 }
